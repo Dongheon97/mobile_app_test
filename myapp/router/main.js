8738b920fe9b27c11fb33e0da69f4fe7def1ac27
dongheon97 <jdd04063@gmail.com>
2021-08-18 18:19:02 +0900

    express.js

---
+++ myapp/router/main.js
@@ -0,0 +1,66 @@
+module.exports = function(app, fs){
+	app.get('/', function(req, res){
+		res.render('index', {
+			title: "Result Page",
+			length: 5
+		})
+	});
+
+	app.get('/list', function(req, res){
+		fs.readFile( __dirname + "/../data/" + "result.json", 'utf8', function (err, data) {
+			console.log( data );
+			res.end( data );
+		});
+	});
+
+	app.get('/result/:num', function(req, res){
+		fs.readFile( __dirname + "/../data/result.json", 'utf8', function (err, data){
+			var appNames = JSON.parse(data);
+			res.json(appNames[req.params.num]);
+		});
+	});
+	
+	app.post('/add/:num', function(req, res){
+		var result = {};
+		var num = req.params.num;
+
+		// Check Validity
+		if(!req.body["appName"]){
+			result["success"] = 0;
+			result["error"] = "Invalid Request";
+			res.json(result);
+			return;
+		}
+
+		// Load Data & Check Dupblication
+		fs.readFile( __dirname + "/../data/result.json", 'utf8', function (err, data){
+			var test = JSON.parse(data);
+			if(test[num]){
+				result["success"] = 0;
+				result["error"] = "duplicate";
+				res.json(result);
+				return;
+			}
+			
+			// Add to Data
+			test[num] = req.body;
+
+			// Save Data
+			fs.writeFile(__dirname + "/../data/result.json",
+						JSON.stringify(test, null, '\t'), 'utf8', function(err,data){
+							result = {"success": 1};
+							res.json(result);
+						})
+		})
+
+	});
+}
+
+/* ############################################################ */
+const add = require('./calc.js').add;
+const sub = require('./calc.js').sub;
+const mult = require('./calc.js').mult;
+const div = require('./calc.js').div;
+/* ############################################################ */
+
+console.log(mult(3, 4), add(2, 4));
---
+++ myapp/tapster/src/clickApp.js
@@ -0,0 +1,124 @@
+var bot = require('./bot.js');
+var waitSync = require('wait-sync');
+
+init = function(){
+    waitSync(0.2)
+    bot.go(0, 0, -140);
+    console.log("Init Coordinate");
+}
+
+click = function(x, y){
+    bot.go(x, y, -155);
+    waitSync(0.2);
+    bot.go(x, y, -167);
+    waitSync(0.2);
+    bot.go(x, y, -155);
+    init();
+    console.log("Click Coordinate: ("+ x + ", "+ y + ")");
+}
+
+swipeRight = function(){
+    bot.go(20, 0, -163);
+    waitSync(0.18);
+    bot.go(-20, 0, -163);
+    init();
+    console.log("Swipe Right");
+}
+
+swipeLeft = function(){
+    bot.go(-20, 0, -163);
+    waitSync(0.18);
+    bot.go(20, 0, -163);
+    init();
+    console.log("Swipe Left");
+}
+
+swipeDown = function(){
+    bot.go(0, -20, -163);
+    waitSync(0.18);
+    bot.go(0, 20, -163);
+    init();
+    console.log("Swipe Down");
+}
+
+swipeUp = function(){
+    bot.go(0, 20, -163);
+    waitSync(0.18);
+    bot.go(0, -20, -163);
+    init();
+    console.log("Swipe Up");
+}
+
+// iOS
+iHome = function(){
+    bot.go(-2, -40, -155);
+    waitSync(0.2);
+    bot.go(-2, -40, -167);
+    waitSync(0.2);
+    bot.go(-2, -40, -155);
+    init();
+    console.log("iOS Home");
+}
+
+iMenu = function(){
+    bot.go(-2, -40, -157);
+    waitSync(0.2);
+    bot.go(-2, -40, -165);
+    waitSync(1);
+    bot.go(-2, -40, -157);
+    init();
+    console.log("iOS Menu")
+}
+
+// Home button Double click
+iDouble_home = function(){
+    bot.go(-2, -40, -155);
+    waitSync(0.2);
+    bot.go(-2, -40, -167);
+    waitSync(0.20);
+    bot.go(-2, -40, -155);
+    waitSync(0.20);
+    bot.go(-2, -40, -167);
+    waitSync(0.20);
+    bot.go(-2, -40, -155);
+    init();
+    console.log("iOS Double Home");
+}
+
+iCoordinates = function(){
+    const xs = 5;
+    const ys = -32;
+    for(var i=0; i<9; i++){
+        bot.go(xs, ys+(7*i), -155);
+        waitSync(1);
+    }
+}
+
+// Android
+aHome = function(){
+    bot.go(0, -56, -150);
+    waitSync(0.2);
+    bot.go(0, -56, -163);
+    waitSync(0.2);
+    bot.go(0, -56, -150);
+    init();
+    console.log("Android Home")
+}
+
+aMenu = function(){
+    bot.go(-27, 25, -150);
+    waitSync(0.2);
+    bot.go(-27, 25, -163);
+    waitSync(0.2);
+    bot.go(0, 25, -163);
+    init();
+    console.log("Android Menu");
+}
+
+module.exports = {
+	iHome,
+	iMenu,
+	iDouble_home,
+	aHome,
+	aMenu
+}
